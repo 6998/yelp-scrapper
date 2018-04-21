f13560bb841e718d840c4c2c7201bfde2eadb466
bnbarak <bn.barak@gmail.com>
2018-04-21 13:18:01 -0400

    working yelp

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const yelp = require('yelp-fusion');
-const client = yelp.client(apiKey);
+const dotenv = require('dotenv')
+
+if(!process.env.ENV) // make sure we have env params
+  dotenv.config()
+
+
+const Yelp = require('yelp-fusion');
+const yelp = Yelp.client(process.env.YELP);
 
 const main = ()=>{
-
+  console.log("=============start=============")
+  yelp.search({
+    term:'Four Barrel Coffee',
+    location: 'san francisco, ca'
+  }).then(response => {
+    console.log(response.jsonBody);
+  }).catch(e => {
+    console.log(e);
+  });
 }
 
+main()
