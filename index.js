f52c7e705b94e2876d98188a6672425b4194fb9d
James Maxwell Stenger <jms2431@columbia.edu>
2018-04-21 13:52:22 -0400

    added categories

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const dotenv = require('dotenv')
 if(!process.env.ENV) // make sure we have env params
   dotenv.config()
 
-
 const Yelp = require('yelp-fusion');
 const yelp = Yelp.client(process.env.YELP);
 
-const main = ()=>{
+const categories = ['chinese, All', 'pizza, All', 'italian, All', 'mexican, All', 'tradamerican']
+const offsets = 
+
+const main = () => {
   console.log("=============start=============")
+
   yelp.search({
-    term:'Four Barrel Coffee',
-    location: 'san francisco, ca'
+    location: 'new york city, ny',
+    categories: 'mexican, All'
   }).then(response => {
+    json = response.jsonBody;
     console.log(response.jsonBody);
   }).catch(e => {
     console.log(e);
