78f761806763937bf7c9a0d43098e5b84ca722c6
bnbarak <bn.barak@gmail.com>
2018-04-21 13:51:18 -0400

    db working

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const dotenv = require('dotenv')
-
-if(!process.env.ENV) // make sure we have env params
+if (!process.env.ENV) // make sure we have env params
   dotenv.config()
 
 
 const Yelp = require('yelp-fusion');
 const yelp = Yelp.client(process.env.YELP);
 
-const main = ()=>{
+const Place = require('./PlaceSchema')
+
+
+const main = () => {
   console.log("=============start=============")
+
+  // Place.create({
+  //   Rating: 123,
+  //   NumberOfReviews: 123,
+  //   Recommended: 13,
+  //   RestaurantId: "test",
+  //   Cuisine: "test"
+  // }, function (err, odie) {
+  //   if (err) {
+  //     return console.log(err);
+  //   } else {
+  //     console.log("Saved")
+  //   }
+  // })
+
+  // Place.get({RestaurantId: "test"}).then(function (place) {
+  //   if (place)
+  //     console.log(place)
+  // }).catch(e => {
+  //   console.log(e)
+  // });
+
+  Place.scan({NumberOfReviews:3}, function(err, place) {
+    if(err) { return console.log(err.message); }
+    console.log(place);
+  });
+
+}
+
+
+const scarpPlaces = () => {
   yelp.search({
-    term:'Four Barrel Coffee',
+    term: 'Four Barrel Coffee',
     location: 'san francisco, ca'
   }).then(response => {
     console.log(response.jsonBody);
@@ -19,4 +52,11 @@ const main = ()=>{
   });
 }
 
+const samePlaces = () => {
+
+}
+
+const sameRandomPlacesToCSV = () => {
+
+}
 main()
